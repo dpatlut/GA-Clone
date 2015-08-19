49ef9ada91003ef7773fb4a6107bf5342e9f5789
maria schettino <mariatschettino@gmail.com>
2015-08-19 12:22:32 -0400

    adding morning examples

---
+++ w08/d03/Instructor/cat.rb
@@ -0,0 +1,89 @@
+require 'pry'
+
+class Cat
+
+	@@cats = []
+
+	def initialize(legs, lives, coat, breed, name)
+		@likes_lasagna = true
+		@legs = legs
+		@lives = lives
+		@coat = coat
+		@breed = breed
+		@name = name
+	end
+
+	#Getters
+		def likes_lasagna
+			@likes_lasagna
+		end
+		def legs
+			@legs
+		end
+		def lives
+			@lives
+		end
+		def coat
+			@coat
+		end
+		def breed
+			@breed
+		end
+		def name
+			@name
+		end
+
+		def self.get_cats
+			@@cats
+		end
+	
+	#Setters
+		def legs=(new_legs)
+			@legs = new_legs
+		end
+		def lives=(new_lives)
+			@lives = new_lives
+		end
+		def coat=(new_coat)
+			@coat= new_coat
+		end
+		def name=(new_name)
+			@name = new_name
+		end
+
+	# instance methods
+
+	def register
+		@@cats.push(self)
+	end
+
+	def alive?
+		# predicate method
+		if @lives > 0
+			true
+		else
+			false
+		end
+	end
+
+	def kill!
+		# self. similar to this
+		if self.alive?
+			@lives -= 1
+		else
+			puts "he gone"
+		end
+	end
+
+	def self.get_cat_name
+		@@cats.each do |cat|
+			puts cat.name
+		end
+	end
+
+end #end Cat
+
+garfield = Cat.new(3, 7, "orange", "American Shorthair", "Garfield")
+all_the_cats = Cat.get_cats
+
+binding.pry
