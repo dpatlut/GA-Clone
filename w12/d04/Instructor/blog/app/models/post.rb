fb1251751f6f5f52f0de54b6b012884979cd3660
Paul Fielek <paulfielek@gmail.com>
2015-09-17 11:45:54 -0400

    added rspec lesson

--- w12/d04/Instructor/blog/app/models/post.rb
+++ w12/d04/Instructor/blog/app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ActiveRecord::Base
 
 	validates :author, presence: true
-	validates :content, presence: true
-	validates_length_of :content, maximum: 500
+	validates :content, presence: true, length: {maximum: 500}
 	validates :title, uniqueness: true
 
 end
--- w12/d04/Instructor/calculator/spec/calculator_spec.rb
+++ w12/d04/Instructor/calculator/spec/calculator_spec.rb
@@ -1,22 +1,22 @@
-require_relative "../models/calculator"
+require_relative "../models/calculator.rb"
 
 describe Calculator do
-
+	let(:calculator) {Calculator.new}
 	describe "#add" do
 		it "should add 2 numbers" do
-			expect(subject.add(2, 2)).to eq 4
+			expect(calculator.add(2, 2)).to eq 4
 		end
 	end
 
 	describe "#subtract" do
 		it "should subtract 2 numbers" do
-			expect(subject.subtract(4, 2)).to eq 2
+			expect(subject.subtract(10, 8)).to eq 2
 		end
 	end
 
 	describe "#multiply" do
 		it "should multiply 2 numbers" do
-			expect(subject.multiply(2, 5)).to eq 10
+			expect(subject.multiply(2, 2)).to eq 4
 		end
 	end
 
@@ -26,7 +26,7 @@ describe Calculator do
 		end
 
 		it "should protect from 0 as a divisor" do
-			expect(subject.divide(5, 0)).to eq "You can't divide by 0!"
+			expect(subject.divide(2, 0)).to eq "You can't divide by 0!"
 		end
 	end
 
